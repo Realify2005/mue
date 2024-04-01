92da248d5d0b4547dbecbde18fb45249a10143d2
Clement Chau <clementtjuwito2@gmail.com>
2024-04-01 18:24:23 +1100

    rename src to app

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/header'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <div className="flex justify-center p-5">
+        <Header />
+      </div>
+    </div>
+  )
+}
+
+export default Home;
