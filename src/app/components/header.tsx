cc57bf0b364a61b944336bcc9fac9b5cf5ff2d63
Clement Chau <clementtjuwito2@gmail.com>
2024-04-01 17:38:31 +1100

    add header.tsx

---
+++ src/app/components/header.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const header: React.FC = () => {
+  return (
+    <div className="relative bg-gray-200 rounded-lg overflow-hidden mx-auto inline-block border border-gray-400">
+      <div className="flex justify-center p-2.5">
+        <ul className="flex gap-4">
+          <li className="transition-colors duration-300 ease-in-out">
+            <a href="#" className="text-black hover:text-white hover:bg-black px-2 py-1 rounded">
+              Home
+            </a>
+          </li>
+          <li className="transition-colors duration-300 ease-in-out">
+            <a href="#" className="text-black hover:text-white hover:bg-black px-2 py-1 rounded">
+              Events
+            </a>
+          </li>
+          <li className="transition-colors duration-300 ease-in-out">
+            <a href="#" className="text-black hover:text-white hover:bg-black px-2 py-1 rounded">
+              Focused Games
+            </a>
+          </li>
+          <li className="transition-colors duration-300 ease-in-out">
+            <a href="#" className="text-black hover:text-white hover:bg-black px-2 py-1 rounded">
+              Membership
+            </a>
+          </li>
+          <li className="transition-colors duration-300 ease-in-out">
+            <a href="#" className="text-black hover:text-white hover:bg-black px-2 py-1 rounded">
+              Our Sponsors
+            </a>
+          </li>
+          <li className="transition-colors duration-300 ease-in-out">
+            <a href="#" className="text-black hover:text-white hover:bg-black px-2 py-1 rounded">
+              Frequently Asked Questions
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default header;
