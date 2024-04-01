667f1bfa91eb6c874256aa825470d55fb7718ce6
Clement Chau <clementtjuwito2@gmail.com>
2024-04-01 19:35:09 +1100

    add logos

---
+++ app/components/MUExUMSU.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Image from 'next/image'
+
+const MUExUMSU: React.FC = () => {
+    return (
+        <div className="flex justify-center align-center">
+            <Image
+                src="/logos/UMSU.webp"
+                alt="UMSU logo"
+                width={275}
+                height={200}
+            />
+            <Image
+                src="/logos/MUE.png"
+                alt="MUE logo"
+                width={200}
+                height={200}
+            />
+        </div>
+    )
+}
+
+export default MUExUMSU
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from './components/header'
+import MUExUMSU from './components/MUExUMSU'
 
 const Home: React.FC = () => {
   return (
-    <div>
+    <div className="grid gap-5 justify-items-center align-items-center">
       <div className="flex justify-center p-5">
         <Header />
       </div>
+      <MUExUMSU />
+      <h2 className="text-4xl font-extrabold dark:text-white">Melbourne University Esports (MUE)</h2>
+      <p className="text-base">The best of Unimelb spirit, now in e-sports</p>
     </div>
   )
 }
