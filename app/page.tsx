38c74414c71c855b271257b15975eb3b178677d3
Clement Chau <clementtjuwito2@gmail.com>
2024-04-02 23:41:03 +1100

    create module LandingSection

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,18 @@
 import React from 'react'
 import Image from 'next/image'
-import Header from './components/Header'
-import MUExUMSU from './components/MUExUMSU'
+import LandingSection from './components/LandingSection'
 import ContactUs from './components/home/ContactUs'
 import Footer from './components/Footer'
 
 const Home: React.FC = () => {
   return (
     <div>
-      <div className="grid gap-5 justify-items-center align-items-center">
-        <div className="flex justify-center p-5">
-          <Header />
-        </div>
-        <MUExUMSU />
-        <h2 className="text-4xl font-extrabold dark:text-white">Melbourne University Esports (MUE)</h2>
-        <p className="text-base">The best of Unimelb spirit, now in e-sports</p>
+      <LandingSection />
+      <div className="flex justify-center items-center">
         <iframe width="560" height="315" src="https://www.youtube.com/embed/Y_fLIxCPSaQ?si=W3tCGvYiruMbPej_" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
       </div>
       <div className="mt-12 px-80 flex flex-col justify-center items-center py-20 bg-cover bg-center bg-no-repeat bg-[url('../public/background/home_bg_1.jpg')]">
-        <h1 className="my-12 text-6xl font-extrabold text-white">Welcome to MUE&apos;ss Official Website</h1>
+        <h1 className="my-12 text-6xl font-extrabold text-white">Welcome to MUE&apos;s Official Website</h1>
         <p className="my-6 text-base text-white">As Unimelb&apos;ss official esports club, our very aim is to promote esports and develop students&apos;s talents in esports that they may have suppressed due to academic commitments. </p>
         <p className="my-6 text-base text-white">On this website, you can check out all about UMESA - our upcoming events, focused games, sponsors&apos;s information and membership benefits, the committee, and even the history of MUE&apos;ss founding! Feel free to take your time to navigate round this site!</p>
       </div>
