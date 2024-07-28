35312c85d98d5354a3d8ad82df27d3262ee5637d
Clement Chau <clementtjuwito2@gmail.com>
2024-07-28 17:40:18 +1000

    add lastUpdatedAt

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import { fetchNotionDatabase } from '../components/api/fetchNotionDatabase';
-import { fetchMostRecentEventsNotionUpdate, mostRecentEventsNotionUpdate } from '../components/api/fetchMostRecentEventsNotionUpdate';
+import { fetchMostRecentEventsNotionUpdate} from '../components/api/fetchMostRecentEventsNotionUpdate';
 import EventCard from '../components/events/EventCard';
 
 export const metadata = {
