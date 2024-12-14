d3037fc88c787458fec3d62b069bdaa0a7c5716e
Jun <yeojy@student.unimelb.edu.au>
2024-12-15 10:58:43 +1100

    Added gradient

--- app/linktree/page.tsx
+++ app/linktree/page.tsx
@@ -8,16 +8,16 @@ const LinkPage = () => {
   return (
     <>
         <Navbar />
-            <div className="mt-12 px-5 py-8 md:px-80 flex flex-col justify-center items-center bg-white">
-            <Image className="rounded-full" src="/logos/MUE_no_words.webp" width={200} height={200} alt="muelogo" />
+            <div className="mt-12 px-5 py-8 md:px-80 flex flex-col justify-center items-center bg-gradient-to-b from-teal-500 to-blue-500">
+            <Image className="rounded-full" src="/logos/MUE_no_words.webp" width={150} height={150} alt="muelogo" />
             <div className='mt-12 flex flex-col justify-center items-center'>
-                    <h1 className='text-3xl'>@MelbUniEsports</h1>
-                    <p className='text-2xl'>Your home of Esports and Gaming at @unimelb! 🏆🎮</p>
+                    <h1 className='text-3xl text-white'>@MelbUniEsports</h1>
+                    <p className='text-2xl text-white'>Your home of Esports and Gaming at @unimelb! 🏆🎮</p>
                 </div>
                 
                 <LinkBar title="🎮MUE Membership 2025! 🎮" link="https://umsu.unimelb.edu.au/buddy-up/clubs/clubs-listing/join/6725/" image="/logos/icons/umsu.png"/>
                 <div className='mt-8 flex flex-col justify-center items-center'>
-                    <p className='text-2xl'>🙌 JOIN US 🙌</p>
+                    <p className='text-2xl text-white'>🙌 JOIN US 🙌</p>
                 </div>
                 <LinkBar title="👾 MUE Discord Server 👾" link="https://discord.gg/VvXuE2NGX6" image="/logos/icons/discord.png"/>
                 <LinkBar title="📸MUE Instagram📸" link="https://www.instagram.com/melbuniesports/" image="/logos/icons/instagram.png"></LinkBar>
@@ -26,7 +26,7 @@ const LinkPage = () => {
                 <LinkBar title="MUE WeChat 微信" link="https://u.wechat.com/kPJgYn2Wn1W2P9MC7gyqKU8" image="/logos/icons/wechat.png"></LinkBar>
                 <LinkBar title="🎬MUE TikTok🎬" link="https://www.tiktok.com/@melbuniesports" image="/logos/icons/tiktok.png"></LinkBar>
                 <div className='mt-8 flex flex-col justify-center items-center'>
-                    <p className='text-2xl'>Contact</p>
+                    <p className='text-2xl text-white'>Contact</p>
                 </div>
                 <LinkBar title="Sponsors/Marketing Contact" link="mailto:publicity.melbuniesports@gmail.com" image=""></LinkBar>
             </div>
