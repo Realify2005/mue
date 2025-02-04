d72950d6e8c89e99a39131b9dd58a6af8793e291
Jun <7540864+junyynyyn@users.noreply.github.com>
2025-02-04 11:15:53 +0800

    Added sorting to FC

--- app/components/teams/teamDisplay.tsx
+++ app/components/teams/teamDisplay.tsx
@@ -5,6 +5,15 @@ import Image from 'next/image';
 
 const TeamDisplay: React.FC<{ game: string, team: string, display: string}> = async ({ game, team, display }) => {
     const data = await fetchTeamInfo(game, team);
+    const ordered_roles = [
+        "Duelist","Initiator", "Controller", "Sentinel",
+        "Top", "Mid", "Jungle", "Bot",
+        "DPS", "Tank", "Support", "Fill",
+        "Player", "Substitute",
+        "Coach"
+    ]
+    data.sort((a,b) => ordered_roles.indexOf(a.role[0]) - ordered_roles.indexOf(b.role[0]));
+    console.log(data);
     return (
       <div className='max-w-md p-2 bg-gray-200'>
         <div>{ display }</div>
@@ -20,6 +29,22 @@ const TeamDisplay: React.FC<{ game: string, team: string, display: string}> = as
       </div>
     )
 }
-  
+
+const valorantDisplay: React.FC<{ team: string, display: string}> = async ({ team, display }) => {
+    const data = await fetchTeamInfo("Valorant", team);
+    const ordered_roles = [
+        "Duelist","Initiator", "Controller", "Sentinel",
+        "Top", "Mid", "Jungle", "Bot",
+        "DPS", "Tank", "Support", "Fill",
+        "Player", "Substitute",
+        "Coach"
+    ]
+    data.sort((a,b) => ordered_roles.indexOf(a.role) - ordered_roles.indexOf(b.role));
+    return (
+        <div>
+
+        </div>
+    );
+}
 
 export default TeamDisplay
