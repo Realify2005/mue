3e87de200766576fe9afd5196015edc6f61160fc
Jun <7540864+junyynyyn@users.noreply.github.com>
2025-02-18 17:27:47 +1100

    Minimum Styling added

--- app/components/teams/teamDisplay.tsx
+++ app/components/teams/teamDisplay.tsx
@@ -13,13 +13,12 @@ const TeamDisplay: React.FC<{ game: string, team: string, display: string}> = as
         "Coach"
     ]
     data.sort((a,b) => ordered_roles.indexOf(a.role[0]) - ordered_roles.indexOf(b.role[0]));
-    console.log(data);
     return (
-      <div className='max-w-md p-2 bg-gray-200'>
-        <div>{ display }</div>
-        <div>
+      <div className='max-w-md p-3 mx-4 bg-gray-200 rounded-lg'>
+        <div className='text-2xl text-center p-1'>{ display }</div>
+        <div className='rounded-lg bg-gray-500 p-1'>
             {data.map(player => {
-                return <div className='bg-gray-500 flex space-x-4 p-1'>
+                return <div className='bg-gray-500 flex space-x-4 p-1' key={player.name}>
                     <p className='min-w-[150px]'>{player.name}</p>
                     <p className='min-w-[100px]'>{player.role}</p>
                     <Image src={player.logo} alt="img" width={50} height={50}/>
@@ -30,21 +29,21 @@ const TeamDisplay: React.FC<{ game: string, team: string, display: string}> = as
     )
 }
 
-const valorantDisplay: React.FC<{ team: string, display: string}> = async ({ team, display }) => {
-    const data = await fetchTeamInfo("Valorant", team);
-    const ordered_roles = [
-        "Duelist","Initiator", "Controller", "Sentinel",
-        "Top", "Mid", "Jungle", "Bot",
-        "DPS", "Tank", "Support", "Fill",
-        "Player", "Substitute",
-        "Coach"
-    ]
-    data.sort((a,b) => ordered_roles.indexOf(a.role) - ordered_roles.indexOf(b.role));
-    return (
-        <div>
+// const valorantDisplay: React.FC<{ team: string, display: string}> = async ({ team, display }) => {
+//     const data = await fetchTeamInfo("Valorant", team);
+//     const ordered_roles = [
+//         "Duelist","Initiator", "Controller", "Sentinel",
+//         "Top", "Mid", "Jungle", "Bot",
+//         "DPS", "Tank", "Support", "Fill",
+//         "Player", "Substitute",
+//         "Coach"
+//     ]
+//     data.sort((a,b) => ordered_roles.indexOf(a.role) - ordered_roles.indexOf(b.role));
+//     return (
+//         <div>
 
-        </div>
-    );
-}
+//         </div>
+//     );
+// }
 
 export default TeamDisplay
--- app/teams/page.tsx
+++ app/teams/page.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image';
 
 import { fetchInfo, fetchTeamInfo } from '../components/api/fetchTeamsInfo';
 import TeamDisplay from '../components/teams/teamDisplay';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 var teams = ["Alpha", "Omega"]
 
@@ -10,9 +12,13 @@ const page = async () => {
     const data = await fetchTeamInfo("Valorant", "Omega");
     return (
         <>
-            <TeamDisplay game="Valorant" team="Omega" display="Team Omega"/>
-            <TeamDisplay game="Counter Strike 2" team="A" display="Counterstrike Team"/>
-            <TeamDisplay game="Overwatch" team="Overwatch_1" display="Overwatch" />
+            <Navbar />
+            <div className="mt-20 flex items-center justify-center">
+                <TeamDisplay game="Valorant" team="Omega" display="Team Omega"/>
+                <TeamDisplay game="Counter Strike 2" team="A" display="Counterstrike Team"/>
+                <TeamDisplay game="Overwatch" team="Overwatch_1" display="Overwatch" />
+            </div>
+            <Footer />
         </>
     );
 }
