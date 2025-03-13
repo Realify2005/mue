d4cb5d9318856313d973f12e99c2f9f46aa10b64
junyynyyn <7540864+junyynyyn@users.noreply.github.com>
2025-03-14 09:03:22 +1100

    Fixed missing some items in notion api breaking the webpage

--- app/components/api/fetchCommitteeInfo.js
+++ app/components/api/fetchCommitteeInfo.js
@@ -6,14 +6,19 @@ export async function fetchCommitteeInfo() {
   const databaseId = process.env.NOTION_COMMITTEE_DATABASE_ID;
   const response = await notion.databases.query({ database_id: databaseId});
 
+  // console.log(response.results);
+  // response.results.forEach(element => {
+  //   console.log(element.properties.Photo.files[0]?.file.url);
+  // });
+
   const indexedData = response.results
     .filter(page => page.properties.Show.checkbox)
     .map(page => {
       return {
         name: page.properties.Name.title[0]?.plain_text,
-        image: page.properties.Photo.files[0].file.url,
+        image: page.properties.Photo.files[0].file?.url,
         department: page.properties.Department.multi_select.map(department => department.name),
-        role: page.properties.Role.select.name,
+        role: page.properties.Role.select?.name,
         about: page.properties.About.rich_text[0]?.plain_text,
         discord: page.properties.Discord.rich_text[0]?.plain_text,
       };
